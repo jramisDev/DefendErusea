1d3b5d8775c88f58f2ec76188f17d09649103e7d
jramisDev <jesus.ramis.galindo@gmail.com>
2023-02-09 22:32:07 +0100

    AudioAnyadido

--- game/src/DefendErusea.cpp
+++ game/src/DefendErusea.cpp
@@ -2,6 +2,44 @@
 #include "screens.h"
 #include <iostream>
 
+class Ship {
+
+    short speed;
+    short health;
+    short powerFire;
+    //Texture2D img;
+    Vector2 currentPos;
+
+public:
+    Ship() {
+        speed = 1;
+        health = 100;
+        powerFire = 1;
+        currentPos.x = 100;
+        currentPos.y = 150;
+    };
+
+    short getSpeed() { return speed; }
+    void setSpeed(short pSpeed) { speed = pSpeed; };
+
+    short getHealth() { return health; }
+    void setHealth(short pDamage) { health = health - pDamage; };
+
+
+    short getPowerFire() { return powerFire; }
+    void setPowerFire(short pPowerFire) { powerFire = pPowerFire; };
+
+    /*Texture2D getImg() { return img; };
+    void setImg(Texture2D pImg) { img = pImg; };*/
+
+    Vector2 getCurrentPosition() { return currentPos; };
+
+    void Move(Vector2 delta) {
+        currentPos.x = currentPos.x + delta.x;
+        currentPos.y = currentPos.y + delta.y;
+    }
+};
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 450;
 static const char* GAME_TITLE = "Defend Erusea";
@@ -41,8 +79,8 @@ int scrollingFore = 0;
 //Posiciones
 Vector2 currentPosition = { 100, 150 };
 
-Rectangle enemy1 = { 400, 250, 60, 15 };
-Rectangle enemy2 = { 400, 100, 90, 35 };
+Rectangle enemy1 = { 450, 250, 60, 15 };
+Rectangle enemy2 = { 600, 100, 90, 35 };
 
 //Velocidad enemigo
 float enemy2Speed = 2.0f;
@@ -79,8 +117,8 @@ void initApp() {
 
     InitAudioDevice();
 
-    damagedSound = LoadSound("resources/sound.wav");
-    diedSound = LoadSound("resources/sound.wav");
+    damagedSound = LoadSound("resources/audio/Impact.wav");
+    diedSound = LoadSound("resources/audio/Explosion.wav");
 }
 
 void endApp() {
@@ -107,7 +145,7 @@ void endApp() {
 
 }
 
-void generateWidgetHealth(int pPlayerHealth) {
+void generateWidgetHealth(short pPlayerHealth) {
 
     //Generamos barra de salud
     if (pPlayerHealth > 50)
@@ -120,7 +158,7 @@ void generateWidgetHealth(int pPlayerHealth) {
 
 int main() {
     
-    
+    Ship playerPlane;
     
     initApp();
 
@@ -168,18 +206,21 @@ int main() {
         //Fin Movimiento de avion
               
         //Enemigos estatico y movible
-        enemy2.x += enemy2Speed;
-        if (enemy2.x >= SCREEN_WIDTH - enemy2.width) enemy2Speed *= -1;
-        if (enemy2.x <= 0) enemy2Speed *= -1;
+        enemy2.y += enemy2Speed;
+        if (enemy2.y >= SCREEN_HEIGHT - enemy2.width) enemy2Speed *= -1;
+        if (enemy2.y <= 0) enemy2Speed *= -1;
 
         //Colision player - enemigo
         Rectangle playerRect = { currentPosition.x, currentPosition.y+20, 100, 30 };
         //DrawRectangleRec(playerRect, WHITE);
 
         if (CheckCollisionRecs(playerRect, enemy1) || CheckCollisionRecs(playerRect, enemy2)) {
-            playerHealth -= 1;
+            playerPlane.setHealth(1);
             
-            playerHealth <= 0 ? PlaySound(diedSound) : PlaySound(damagedSound);
+            if(!IsSoundPlaying(damagedSound) && !IsSoundPlaying(diedSound)){
+                playerPlane.getHealth() <= 0 ? PlaySound(diedSound) : PlaySound(damagedSound);
+            }
+            
         }
         //FIN Colision player - enemigo
 
@@ -196,7 +237,7 @@ int main() {
         DrawTextureEx(greenPlane, currentPosition, 0.0f, 0.1f, WHITE);
 
 
-        generateWidgetHealth(playerHealth);
+        generateWidgetHealth(playerPlane.getHealth());
 
 
         EndDrawing();
