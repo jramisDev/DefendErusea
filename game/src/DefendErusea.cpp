692f2c3b0fff6442e167d44e6e6976dc1a69fadc
jramisDev <jesus.ramis.galindo@gmail.com>
2023-02-10 01:51:49 +0100

    Funcional

--- game/src/DefendErusea.cpp
+++ game/src/DefendErusea.cpp
@@ -26,23 +26,25 @@
 
 class Ship {
 
-    short speed;
+    short currentSpeed;
     short health;
     short powerFire;
-    //Texture2D img;
+
+    Texture2D img;
+
     Vector2 currentPos;
 
 public:
     Ship() {
-        speed = 3;
+        currentSpeed = 10;
         health = 100;
         powerFire = 1;
         currentPos.x = 0;
-        currentPos.y = 150;
+        currentPos.y = 100;
     };
 
-    short getSpeed() { return speed; }
-    void setSpeed(short pSpeed) { speed = pSpeed; };
+    short getCurrentSpeed() { return currentSpeed; }
+    void setCurrentSpeed(short pSpeed) { currentSpeed = pSpeed; };
 
     short getHealth() { return health; }
     void setHealth(short pDamage) { health = health - pDamage; };
@@ -51,14 +53,15 @@ public:
     short getPowerFire() { return powerFire; }
     void setPowerFire(short pPowerFire) { powerFire = pPowerFire; };
 
-    /*Texture2D getImg() { return img; };
-    void setImg(Texture2D pImg) { img = pImg; };*/
+    Texture2D getImg() { return img; };
+    void setImg(Texture2D pImg) { img = pImg; };
 
     Vector2 getCurrentPosition() { return currentPos; };
+    void setCurrentPosition(Vector2 pPosition) { currentPos = pPosition; }
 
     void Move(Vector2 delta) {
-        currentPos.x = currentPos.x + delta.x;
-        currentPos.y = currentPos.y + delta.y;
+        currentPos.x = currentPos.x + (delta.x * currentSpeed);
+        currentPos.y = currentPos.y + (delta.y * currentSpeed);
     }
 };
 
@@ -94,7 +97,6 @@ int scrollingMid = 0;
 int scrollingFore = 0;
 
 //Posiciones
-Vector2 currentPosition = { 0, 150 };
 
 Rectangle enemy1 = { 450, 250, 60, 15 };
 Rectangle enemy2 = { 600, 100, 90, 35 };
@@ -195,55 +197,95 @@ void setBackground(void) {
     //Fin background
 }
 
-void setMovementPlayer(Ship pPlayerPlane) {
-
-    Vector2 movement = { 0,0 };
+void setMovementPlayer(Ship pPlayerPlane, Vector2 pMovement) {
 
     //Movimiento de avion
-    if (IsKeyDown(KEY_A)) {
-        if (currentPosition.x >= 2) currentPosition.x = currentPosition.x - pPlayerPlane.getSpeed();
+   /* if (IsKeyDown(KEY_A)) {
+        if (currentPosition.x >= 2) currentPosition.x = currentPosition.x - pPlayerPlane.getCurrentSpeed();
     }
     if (IsKeyDown(KEY_D)) {
-        if (currentPosition.x <= 698) currentPosition.x = currentPosition.x + pPlayerPlane.getSpeed();
+        if (currentPosition.x <= 698) currentPosition.x = currentPosition.x + pPlayerPlane.getCurrentSpeed();
     }
     if (IsKeyDown(KEY_W)) {
-        if (currentPosition.y >= 2) currentPosition.y = currentPosition.y - pPlayerPlane.getSpeed();
+        if (currentPosition.y >= 2) currentPosition.y = currentPosition.y - pPlayerPlane.getCurrentSpeed();
     }
     if (IsKeyDown(KEY_S)) {
-        if (currentPosition.y <= 390) currentPosition.y = currentPosition.y + pPlayerPlane.getSpeed();
+        if (currentPosition.y <= 390) currentPosition.y = currentPosition.y + pPlayerPlane.getCurrentSpeed();
+    }*/
+
+    if (IsKeyDown(KEY_A)) {
+        if (pPlayerPlane.getCurrentPosition().x >= 2)  pMovement.x = -1;
     }
+    if (IsKeyDown(KEY_D)) {
+        if (pPlayerPlane.getCurrentPosition().x <= 698) pMovement.x = 1;
+    }
+    if (IsKeyDown(KEY_W)) {
+        if (pPlayerPlane.getCurrentPosition().y >= 2) pMovement.y = -1;
+    }
+    if (IsKeyDown(KEY_S)) {
+        if (pPlayerPlane.getCurrentPosition().y <= 385) pMovement.y = 1;
+    }
+    pPlayerPlane.Move(pMovement);
 
     //Fin Movimiento de avion
 }
 
-void setMovementEnemy() {
+void setMovementEnemy(Ship pEnemyMove) {
     //Enemigos estatico y movible
     enemy2.y += enemy2Speed;
     if (enemy2.y >= SCREEN_HEIGHT - enemy2.width) enemy2Speed *= -1;
     if (enemy2.y <= 0) enemy2Speed *= -1;
 }
 
-void setGenerateProgressionBar() {
-    DrawRectangle(0, SCREEN_HEIGHT - 5, currentPosition.x + 100, 5, BLACK);
+void setGenerateProgressionBar(Ship pPlayer) {
+    DrawRectangle(0, SCREEN_HEIGHT - 5, int(pPlayer.getCurrentPosition().x) + 100, 5, BLACK);
 }
 
 int main() {
     
-    Ship playerPlane;
-    Ship enemyOne;
-    enemyOne.setPowerFire(1);
-    Ship enemyTwo;
-    enemyTwo.setPowerFire(2);
-    
     initApp();
 
+    Ship playerPlane;
+    playerPlane.setImg(greenPlane);    
+
+
+    Ship enemyStatic;
+    enemyStatic.setPowerFire(1);
+    enemyStatic.setImg(bombStatic);
+    enemyStatic.setCurrentPosition({ 430, 235 });
+
+
+    Ship enemyMove;
+    enemyMove.setPowerFire(2);
+    enemyMove.setImg(bombMove);
+    enemyMove.setCurrentPosition({ 595, 100 });
+
     int framesCounter = 0;
-    bool inicio = true;
 
 
     while (!WindowShouldClose()) {
 
         framesCounter++;
+
+        Vector2 movement;
+        movement.x = 0;
+        movement.y = 0;
+
+        if (IsKeyDown(KEY_A)) {
+            if (playerPlane.getCurrentPosition().x >= 2)  movement.x = -1;
+        }
+        if (IsKeyDown(KEY_D)) {
+            if (playerPlane.getCurrentPosition().x <= 698) movement.x = 1;
+        }
+        if (IsKeyDown(KEY_W)) {
+            if (playerPlane.getCurrentPosition().y >= 2) movement.y = -1;
+        }
+        if (IsKeyDown(KEY_S)) {
+            if (playerPlane.getCurrentPosition().y <= 385) movement.y = 1;
+        }
+        playerPlane.Move(movement);
+
+        //setMovementPlayer(playerPlane, movement);
 
         BeginDrawing();
 
@@ -251,23 +293,20 @@ int main() {
 
         setBackground();
 
-        setMovementPlayer(playerPlane);
+        
 
-        setMovementEnemy();
+        setMovementEnemy(enemyMove);
               
-        
 
-        
         //Controlamos que nos de un margen de tiempo tras impactos
-        if (inicio || framesCounter >= FPS) {
+        if (framesCounter >= 30) {
 
             //Colision player - enemigo
-            Rectangle playerRect = { currentPosition.x, currentPosition.y + 20, 100, 30 };
-            //DrawRectangleRec(playerRect, WHITE);
+            Rectangle playerRect = { playerPlane.getCurrentPosition().x, playerPlane.getCurrentPosition().y + 20, 100, 30 };
 
 
             if (CheckCollisionRecs(playerRect, enemy1)) {
-                playerPlane.setHealth(enemyOne.getPowerFire());
+                playerPlane.setHealth(enemyStatic.getPowerFire());
 
                 if (!IsSoundPlaying(damagedSound) && !IsSoundPlaying(diedSound)) {
                     playerPlane.getHealth() <= 0 ? PlaySound(diedSound) : PlaySound(damagedSound);
@@ -276,7 +315,7 @@ int main() {
             }
 
             if (CheckCollisionRecs(playerRect, enemy2)) {
-                playerPlane.setHealth(enemyTwo.getPowerFire());
+                playerPlane.setHealth(enemyMove.getPowerFire());
 
                 if (!IsSoundPlaying(damagedSound) && !IsSoundPlaying(diedSound)) {
                     playerPlane.getHealth() <= 0 ? PlaySound(diedSound) : PlaySound(damagedSound);
@@ -285,27 +324,27 @@ int main() {
             }
             //FIN Colision player - enemigo
             framesCounter = 0;
-            inicio = false;
         }
 
 
-        //DrawRectangleRec(enemy1, WHITE);
-        DrawTextureEx(bombStatic, { enemy1.x-20, enemy1.y-15 }, 0.0f, 0.2f, WHITE);
+        //Enemigos
+        DrawTextureEx(enemyStatic.getImg(), enemyStatic.getCurrentPosition(), 0.0f, 0.2f, WHITE);
 
-        //DrawRectangleRec(enemy2, WHITE);
-        DrawTextureEx(bombMove, { enemy2.x-5, enemy2.y }, 0.0f, 0.2f, WHITE);
-        //Fin enemigos
+        DrawTextureEx(enemyMove.getImg(), { enemy2.x - 5, enemy2.y }, 0.0f, 0.2f, WHITE);
+       //Fin enemigos
 
         //Generamos el nuestro avion
-        DrawTextureEx(greenPlane, currentPosition, 0.0f, 0.1f, WHITE);
+        //DrawCircle((int)playerPlane.getCurrentPosition().x, (int)playerPlane.getCurrentPosition().y, 10, BLUE);
+        DrawTextureEx(playerPlane.getImg(), playerPlane.getCurrentPosition(), 0.0f, 0.1f, WHITE);
 
         generateWidgetHealth(playerPlane.getHealth());
 
-        setGenerateProgressionBar();
-        
+        //Revisar, no esta correcto
+        setGenerateProgressionBar(playerPlane);
 
         EndDrawing();
     }
+
 
     endApp();
 
